c00b3f18c7203fbfc6e2af87776cfb30ae952e6a
AgmarTorres <agmartorres@gmail.com>
2018-10-15 05:33:22 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+const imprimirResultado = function(num){
+    if(num >= 7){
+        console.log("Aprovado")
+    }
+    else{console.log("Reprovado")}
+}
+
+imprimirResultado(7)
+imprimirResultado(4)
+imprimirResultado("Epa!")// Cuidado com a tipagem.
