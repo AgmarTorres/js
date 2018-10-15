9c8a3fc4c5108cce7eb466e7a2b8c921757fc0c9
AgmarTorres <agmartorres@gmail.com>
2018-10-15 05:34:07 -0300

    Add files via upload

---
+++ controle/if1.js
@@ -0,0 +1,24 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log("Aprovado");
+    }
+}
+soBoaNoticia(8.1)
+function seForVerdadeiroEuFalo(){
+    if(valor){
+        console.log('É verdade... ' + valor)
+    }
+}
+
+seForVerdadeiroEuFalo()
+seForVerdadeiroEuFalo(null)
+seForVerdadeiroEuFalo(undefined)
+seForVerdadeiroEuFalo(NaN)
+seForVerdadeiroEuFalo('')
+seForVerdadeiroEuFalo(0)
+seForVerdadeiroEuFalo(-1)
+seForVerdadeiroEuFalo(' ')
+seForVerdadeiroEuFalo('?')
+seForVerdadeiroEuFalo([])
+seForVerdadeiroEuFalo({})
+seForVerdadeiroEuFalo([1,2])
